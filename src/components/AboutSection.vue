1e6c2102c65b1b64d2443f0826dc3df74e5ea43a
lokraj thapa <lokrajthapa741@gmail.com>
2024-10-20 09:31:51 +0545

    portfolio website

---
+++ src/components/AboutSection.vue
@@ -0,0 +1,79 @@
+<template >
+    <section class="text-white mt-18" id="about">
+        <div class="absolute z-0 top-[93rem] inset-x-0 h-64 flex items-start">
+            <div class="h-24 w-64 bg-gradient-to-br from-primary via-secondary blur-2xl to-[#570cac] opacity-20"></div>
+        </div>
+        <div class="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16 z-1">
+            <div data-aos="flip-right">
+                <h2 class="text-4xl font-bold text-white text-left mb-8">My Education</h2>
+                <div class="space-y-8 py-8">
+                    <div v-for="element in education" :key="element.id"
+                    class="flex items-center md:w-[80%] w-full rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641]"
+                    >
+                        <div class="w-1/4">
+                            <img src="https://img.icons8.com/ios-glyphs/60/ffffff/graduation-cap--v1.png" alt="graduation-cap--v1">
+                        </div>
+                        <div class="w-3/4 pl-4">
+                            <h3 class="text-2xl font-semibold uppercase text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary lg:text-xl">
+                                {{ element.School }}
+                            </h3>
+                            <p class="text-white">{{ element.program }}</p>
+                            <p class="text-white">{{ element.year }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-2 md:mt-0 text-left flex flex-col z-10 h-full" data-aos="flip-right">
+                <h2 class="text-4xl font-bold text-white md:text-center text-left mb-4 ">More
+                    <span class="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">About</span>
+                    Me
+                </h2>
+                <p class="text-base lg:text-lg mt-8 py-8"> I am a dedicated full-stack developer with 3 years of experience, 
+                    specializing in PHP development with Laravel on the backend and Vue.js for crafting dynamic, user-friendly
+                     front-end interfaces. I have a strong foundation in PHP, enabling me to build robust, scalable web applications,
+                      develop RESTful APIs, and efficiently manage databases. My expertise in integrating Vue.js with Laravel ensures
+                       seamless and responsive user experiences. Proficient in tools like Tailwind CSS, Git, and Docker, I thrive in collaborative environments, delivering clean, 
+                    well-structured code and innovative solutions for complex challenges
+                </p>
+                <div class="grid grid-cols-3 gap-4 max-w-lg pt-8">
+                    <div class="text-center rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641] p-3">
+                        <h3 class="text-white font-bold text-xl sm:text-2xl lg:text-3xl">+20</h3>
+                        <p class="text-sm sm:text-base text-gray-300">Happy Client</p>
+                    </div>
+                    <div class="text-center rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641] p-3">
+                        <h3 class="text-white font-bold text-xl sm:text-2xl lg:text-3xl">+30</h3>
+                        <p class="text-sm sm:text-base text-gray-300">Project</p>
+                    </div>
+
+                    <div class="text-center rounded-xl bg-[#111a3e] shadow-lg border border-[#1f1641] p-3">
+                        <h3 class="text-white font-bold text-xl sm:text-2xl lg:text-3xl">+3</h3>
+                        <p class="text-sm sm:text-base text-gray-300"> Years Experience</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+<script setup>
+import { ref } from 'vue';
+const education=ref([
+    {
+        id:1,
+        School:'social public school',
+        program:' Secondary Education',
+        year:'2015'
+    },
+    {
+        id:2,
+        School:' Kalika Multiple Campus',
+        program:'Science',
+        year:'2017'
+    },
+    {
+        id:2,
+        School:' Mount  Annapurna Campus',
+        program:'Bsc.CSIT (Bachelor of science, computer science and Information Technology )',
+        year:'2021'
+    }
+])
+</script>
